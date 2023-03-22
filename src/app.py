76a73d30fcfbbff942f58d87af1f2f999780241f
Tianyi Pan <tianyi.pan@clobotics.com>
2023-03-22 22:34:31 +0800

    Adjust prompts

--- src/app.py
+++ src/app.py
@@ -266,7 +266,7 @@ st.markdown(get_css(), unsafe_allow_html=True)
 # Initialize/maintain a chat log and chat memory in Streamlit's session state
 # Log is the actual line by line chat, while memory is limited by model's maximum token context length
 if "MEMORY" not in st.session_state:
-    st.session_state.MEMORY = [{'role': "user", 'content': INITIAL_PROMPT}]
+    st.session_state.MEMORY = [{'role': "system", 'content': INITIAL_PROMPT}]
     st.session_state.LOG = [INITIAL_PROMPT]
 
 
