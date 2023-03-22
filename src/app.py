d9b6263241aa82a2cb5980c893e135465505730f
Tianyi Pan <60060750+tipani86@users.noreply.github.com>
2023-03-22 15:32:04 +0800

    Update counter (#5)
    Co-authored-by: Tianyi Pan <tianyi.pan@clobotics.com>

--- src/app.py
+++ src/app.py
@@ -249,7 +249,7 @@ footer = st.container()
 with footer:
     st.markdown("""
     <div align=right><small>
-    Unique visitors: <img src="https://www.cutercounter.com/hits.php?id=hxndkpk&nd=5&style=1" border="0" alt="website counter"><br>
+    Unique visitors: <img src="https://www.cutercounter.com/hits.php?id=hxndkqx&nd=5&style=2" border="0" alt="website counter"><br>
     GitHub <a href="https://github.com/tipani86/CatGDP"><img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/tipani86/CatGDP?style=social"></a>
     </small></div>
     """, unsafe_allow_html=True)
