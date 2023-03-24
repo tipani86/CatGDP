0be047771d2cda68efc926f8a82d83b257ebb5f3
Tianyi Pan <tianyi.pan@clobotics.com>
2023-03-24 11:03:22 +0800

    Fix typo

--- src/app.py
+++ src/app.py
@@ -249,7 +249,7 @@ footer = st.container()
 with footer:
     st.markdown("""
     <div align=right><small>
-    Page views: <img src="https://www.cutercounter.com/hits.php?id=hvxndaff&nd=6&style=1" border="0" alt="hit counter"<br>
+    Page views: <img src="https://www.cutercounter.com/hits.php?id=hvxndaff&nd=6&style=1" border="0" alt="hit counter"><br>
     Unique visitors: <img src="https://www.cutercounter.com/hits.php?id=hxndkqx&nd=4&style=2" border="0" alt="website counter"><br>
     GitHub <a href="https://github.com/tipani86/CatGDP"><img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/tipani86/CatGDP?style=social"></a>
     </small></div>
